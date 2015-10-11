6b364cb18c2d49f365f6976af00870bff1cc2edb
v-nayjack <vinay.jakkali@gmail.com>
2015-10-11 19:31:33 -0400

    plot files

---
+++ Plot2.R
@@ -0,0 +1,20 @@
+library(dplyr) ## loading the library to read the data
+
+## reading the data
+powerdata <- tbl_df(read.table ("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = c("?","")))
+
+## Coverting the date column
+powerdata$Date <- as.Date(power_data$Date, format = "%d/%m/%Y")
+
+## converting the time to POSIXIlt
+power_data$temptime <- paste(power_data$Date, power_data$Time)
+power_data$Time <- strptime(power_data$temptime, format = "%Y-%m-%d %H:%M:%S")
+
+## subsetting the data
+data <- subset(powerdata, Date>="2007-02-01" & Date<="2007-02-02")
+## saving the png file
+png("Plot2.png", width = 480, height = 480, units = "px")
+## creating the histogram for plot1
+plot(data$Global_active_power, data$Date, xlab = c("Thu", "Fri", "sat"), ylab = "Global Active Power (kilowatts)")
+
+dev.off()
